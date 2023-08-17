ea1c5a9119e8a41cd0efd7aac5aadc8140229f87
blanc <smu08054@gmail.com>
2023-08-17 22:25:54 +0900

    SSL 적용, nginx 프록시 사용으로 인한 포트 변경(80->3000)

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62776fe6f54e1896"; }
+/******/ 		__webpack_require__.h = function() { return "7187d8ff8cbed495"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
