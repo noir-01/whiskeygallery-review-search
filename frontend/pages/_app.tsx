543ce6a6243e37150ba1feb190307193a2085741
seung1 <97tmddnjs@naver.com>
2023-09-05 03:42:39 +0900

    오류메세지 스낵바 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import CssBaseline from "@mui/material/CssBaseline";
+import { SnackbarProvider, closeSnackbar } from "notistack";
+import { CssBaseline, IconButton } from "@mui/material";
+import { Close as CloseIcon } from "@mui/icons-material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App = (props: AppProps) => {
@@ -9,7 +11,7 @@ const App = (props: AppProps) => {
   const meta = {
     description: "위스키 리뷰 검색 & 작성 사이트",
     title: "Whiskey Gallery",
-    // url: "",
+    url: "https://whiskeyreview.ddns.net",
     // image:"",
   };
 
@@ -24,8 +26,9 @@ const App = (props: AppProps) => {
         />
         <title>{meta.title}</title>
         <meta name="title" content={meta.title} key="title" />
-
         <meta name="description" content={meta.description} key="description" />
+        <meta name="theme-color" content="#755139" key="themeColor" />
+        <link rel="canonical" href={meta.url} />
 
         <meta property="og:title" content={meta.title} key="og_title" />
         <meta
@@ -34,6 +37,7 @@ const App = (props: AppProps) => {
           key="og_description"
         />
         <meta property="og:site_name" content={meta.title} key="og_site_name" />
+        <meta property="og:url" content={meta.url} key="og_url" />
 
         <meta name="twitter:title" content={meta.title} key="twt_title" />
         <meta
@@ -41,6 +45,7 @@ const App = (props: AppProps) => {
           content={meta.description}
           key="twt_description"
         />
+        <meta name="twitter:url" content={meta.url} key="twt_url" />
 
         <meta
           name="apple-mobile-web-app-title"
@@ -59,9 +64,22 @@ const App = (props: AppProps) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <CssBaseline />
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <SnackbarProvider
+        action={(snackbarId) => (
+          <IconButton onClick={() => closeSnackbar(snackbarId)} color="inherit">
+            <CloseIcon />
+          </IconButton>
+        )}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        preventDuplicate={true}
+      >
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </SnackbarProvider>
     </>
   );
 };
