e2cc4d72466f2bd36dd37e0bb8aa99a8ebe7f68c
noir-01 <smu08054@gmail.com>
2025-02-14 23:48:06 +0900

    Back/Build: 스프링 서버 초기 설정

---
+++ backend/review-api/src/test/java/com/whiskeygallery_review/review_api/ReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiskeygallery_review.review_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
