73f3776dfb62310221213dc7b0713be79f2327b6
noir-01 <smu08054@gmail.com>
2025-02-14 23:48:06 +0900

    Back/Build: 스프링 서버 초기 설정

---
+++ backend/review-api/src/main/java/com/whiskeygallery_review/review_api/ReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.whiskeygallery_review.review_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApiApplication.class, args);
+	}
+
+}
