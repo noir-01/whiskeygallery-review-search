7f829bb416563f03c05d90e0c8c7ea9b8a25f286
noir-01 <smu08054@gmail.com>
2025-02-17 00:06:02 +0900

    Back/Feat: 위스키 리뷰 페이징 적용

--- backend/review-api/src/main/java/com/whiskeygallery_review/review_api/dto/ReviewDto.java
+++ backend/review-api/src/main/java/com/whiskeygallery_review/review_api/dto/ReviewDto.java
@@ -2,17 +2,19 @@ package com.whiskeygallery_review.review_api.dto;
 
 import com.whiskeygallery_review.review_api.entity.OtherReview;
 import com.whiskeygallery_review.review_api.entity.WhiskeyReview;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import java.time.LocalDate;
 
 @Getter
+@AllArgsConstructor
 public class ReviewDto {
     private Integer id;
     private String title;
     private Integer recommend;
     private Integer reply;
     private String nickname;
-    private LocalDate time;
+    private LocalDate postDate;
     private String category;
 
     public ReviewDto(WhiskeyReview review) {
@@ -21,7 +23,7 @@ public class ReviewDto {
         this.recommend = review.getRecom();
         this.reply = review.getReply();
         this.nickname = review.getNickname();
-        this.time = review.getTime();
+        this.postDate = review.getPostDate();
         this.category = "whiskey";
     }
 
@@ -31,7 +33,7 @@ public class ReviewDto {
         this.recommend = review.getRecom();
         this.reply = review.getReply();
         this.nickname = review.getNickname();
-        this.time = review.getTime();
+        this.postDate = review.getPostDate();
         this.category = review.getCategory();
     }
 }
--- backend/review-api/src/main/java/com/whiskeygallery_review/review_api/entity/WhiskeyReview.java
+++ backend/review-api/src/main/java/com/whiskeygallery_review/review_api/entity/WhiskeyReview.java
@@ -3,9 +3,22 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @Entity
 @Table(name = "whiskeyReview")
-public class WhiskeyReview extends BaseReview {
+//public class WhiskeyReview extends BaseReview {
+//}
+public class WhiskeyReview{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private Integer recom;
+    private Integer reply;
+    private LocalDate postDate;
+    private String nickname;
 }
