a40bfcd3fbe6f99d024165e656ec3ee3d80a4727
noir-01 <smu08054@gmail.com>
2025-02-23 21:32:30 +0900

    Back/Fix: CORS 설정

--- backend/review-api/src/main/java/com/whiskeygallery_review/review_api/config/WebConfig.java
+++ backend/review-api/src/main/java/com/whiskeygallery_review/review_api/config/WebConfig.java
@@ -13,10 +13,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 // 프론트엔드에서 오는 요청을 허용
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000") // 프론트엔드 URL
-                        .allowedMethods("GET")
-                        .allowCredentials(true);  // 인증이 필요한 요청도 허용
+                registry.addMapping("/api/review/**")
+                        .allowedOrigins("*") // 프론트엔드 URL
+                        .allowedMethods("GET");
             }
         };
     }
