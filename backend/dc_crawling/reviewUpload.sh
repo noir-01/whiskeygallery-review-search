419ccd78a42d7a13ac4c725f94c3b808cf88ad6c
noir-01 <smu08054@gmail.com>
2025-04-27 00:59:57 +0900

    Chore: 경로 이름 변경

---
+++ backend/dc_crawling/reviewUpload.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+. /home/blanc/whiskey-venv/bin/activate
+python /home/blanc/whiskey-venv/whiskey/backend/dc_crawling/crawl.py
+
+#crontab
+# 0 5 * * * /home/blanc/whiskey-venv/whiskey/backend/dccrolling/reviewUpload.sh > /home/blanc/whiskey-venv/whiskey/backend/dccrolling/reviewUpload.log 2>&1
+# * * * * * /home/blanc/Documents/monitoring/monitoring.sh > /home/blanc/Documents/monitoring/monitoring.log 2>&1
+# 0 14 * * tue,fri lp /home/blanc/test.gif
+
+#sudo crontab
+# 0 4 * * * /sbin/shutdown -r now
+# 0 5 1 */2 * /usr/local/bin/renew_cert.sh >> /var/log/cert_renewal.log 2>&1
+
+#renew_cert.sh
+#!/bin/bash
+# sudo service nginx stop
+# sudo certbot renew --dry-run
+# sudo certbot renew
+# sudo service nginx start
+
+#monitoring/monitoring.sh
+#cd /home/blanc/Documents/monitoring
+#python monitoring.py
