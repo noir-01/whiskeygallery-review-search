676b9efcec874535c5ac7aa369bdee57a513a727
noir-01 <smu08054@gmail.com>
2025-05-30 22:31:30 +0900

    Feat: Mroonga FullText Index로 검색

--- backend/review-api/src/main/java/com/whiskeygallery_review/review_api/repository/impl/OtherReviewRepositoryImpl.java
+++ backend/review-api/src/main/java/com/whiskeygallery_review/review_api/repository/impl/OtherReviewRepositoryImpl.java
@@ -4,9 +4,10 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.whiskeygallery_review.review_api.entity.OtherReview;
 import com.whiskeygallery_review.review_api.entity.QOtherReview;
 import com.whiskeygallery_review.review_api.repository.BaseReviewCustomRepository;
+import jakarta.persistence.EntityManager;
 
 public class OtherReviewRepositoryImpl extends BaseReviewCustomRepositoryImpl<OtherReview> implements BaseReviewCustomRepository<OtherReview> {
-    public OtherReviewRepositoryImpl(JPAQueryFactory queryFactory) {
-        super(queryFactory, QOtherReview.otherReview, QOtherReview.otherReview.title, QOtherReview.otherReview.nickname);
+    public OtherReviewRepositoryImpl(JPAQueryFactory queryFactory, EntityManager entityManager) {
+        super(queryFactory, entityManager,QOtherReview.otherReview, QOtherReview.otherReview.title, QOtherReview.otherReview.nickname);
     }
 }
--- backend/review-api/src/main/java/com/whiskeygallery_review/review_api/repository/impl/WhiskeyReviewRepositoryImpl.java
+++ backend/review-api/src/main/java/com/whiskeygallery_review/review_api/repository/impl/WhiskeyReviewRepositoryImpl.java
@@ -4,9 +4,13 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.whiskeygallery_review.review_api.entity.QWhiskeyReview;
 import com.whiskeygallery_review.review_api.entity.WhiskeyReview;
 import com.whiskeygallery_review.review_api.repository.BaseReviewCustomRepository;
+import jakarta.persistence.EntityManager;
 
 public class WhiskeyReviewRepositoryImpl extends BaseReviewCustomRepositoryImpl<WhiskeyReview> implements BaseReviewCustomRepository<WhiskeyReview> {
-    public WhiskeyReviewRepositoryImpl(JPAQueryFactory queryFactory) {
-        super(queryFactory, QWhiskeyReview.whiskeyReview, QWhiskeyReview.whiskeyReview.title, QWhiskeyReview.whiskeyReview.nickname);
+    public WhiskeyReviewRepositoryImpl(
+        JPAQueryFactory queryFactory,
+        EntityManager entityManager
+        ) {
+        super(queryFactory, entityManager, QWhiskeyReview.whiskeyReview, QWhiskeyReview.whiskeyReview.title, QWhiskeyReview.whiskeyReview.nickname);
     }
 }
